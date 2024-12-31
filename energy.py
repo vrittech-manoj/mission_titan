2623b997362d64f374d78d35866e8db700c7a3bd
Sushil Shrestha <shresthasushil6341@gmail.com>
2024-12-31 22:27:06 +0545

    energy source update by Sushil Shrestha

---
+++ energy.py
@@ -0,0 +1,23 @@
+class Fuel:
+    
+    def __init__(self,status): #Fuel class
+          self.status =status
+    
+    def check_fuel(self): #Check fuel Status
+        pass
+    
+    def refuel(self): #Fuel refuel
+        pass
+    
+
+class Repair:
+    def __init__(self): #use any tool is required
+        pass
+    
+    def energy_source(): # available energy source
+        pass
+    
+    def use_energy(): # to ti available energy source
+        pass
+
+    
